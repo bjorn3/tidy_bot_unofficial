994d15a5f90b1a87c190434c313d4388529172e5
bjorn3 <bjorn3@users.noreply.github.com>
2019-07-21 17:33:00 +0200

    Move clone_repo and run_tidy to check.rs

---
+++ src/check.rs
@@ -0,0 +1,102 @@
+use std::fs;
+use std::io;
+use std::process::{Command, Stdio};
+
+pub struct Error {
+    pub message: String,
+    pub file_and_line: Option<(String, u64)>,
+}
+
+pub fn run_tidy() -> Vec<Error> {
+    let output = Command::new("../tidy")
+        .current_dir("rust")
+        .arg("src")
+        .arg("cargo")
+        .arg("--no-vendor")
+        .stdout(Stdio::piped())
+        .output()
+        .unwrap();
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let stderr = String::from_utf8(output.stderr).unwrap();
+
+    let result = stdout + &stderr;
+
+    let line_too_long_regex =
+        regex::Regex::new(r#"tidy error: ([\w\d/\.-_]+):(\d+): line longer than 100 chars"#)
+            .unwrap();
+
+    let mut errors = Vec::new();
+
+    for line in result.lines() {
+        if line == "some tidy checks failed" {
+        } else if let Some(captures) = line_too_long_regex.captures(line) {
+            errors.push(Error {
+                message: "line longer than 100 chars".to_string(),
+                file_and_line: Some((captures[1].to_string(), captures[2].parse().unwrap())),
+            })
+        } else {
+            errors.push(Error {
+                message: line.to_string(),
+                file_and_line: None,
+            })
+        }
+    }
+
+    errors
+}
+
+macro_rules! cmd {
+    (@($wd:expr) $cmd:ident $($args:expr),*) => {{
+        let mut cmd = Command::new(stringify!($cmd));
+        cmd.current_dir($wd);
+        $(
+            cmd.arg($args);
+        )*
+        cmd.status()
+    }};
+    ($cmd:ident $($args:expr),*) => {
+        cmd!(@(".") $cmd $($args),*);
+    };
+}
+
+pub fn clone_repo(repo: &str, commit: &str) -> io::Result<()> {
+    if fs::read_dir("rust").is_err() {
+        println!("===> Cloning https://github.com/rust-lang/rust.git");
+        cmd!(git "clone", "https://github.com/rust-lang/rust.git")?;
+    }
+
+    let repo_id = {
+        use std::hash::{Hash, Hasher};
+
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        repo.hash(&mut hasher);
+        hasher.finish().to_string()
+    };
+
+    println!("===> Cloning {}", repo);
+
+    let known_remotes = String::from_utf8(
+        Command::new("git")
+            .current_dir("rust")
+            .arg("remote")
+            .stdout(Stdio::piped())
+            .output()?
+            .stdout,
+    )
+    .unwrap();
+
+    if known_remotes
+        .split('\n')
+        .find(|repo| repo == &repo_id)
+        .is_none()
+    {
+        cmd!(@("rust") git "remote", "add", &repo_id, repo)?;
+    }
+    cmd!(@("rust") git "fetch", repo_id)?;
+    cmd!(@("rust") git "checkout", commit)?;
+
+    println!("===> Checked out {}", commit);
+
+    Ok(())
+}
